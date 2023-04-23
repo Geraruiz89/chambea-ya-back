dec686a876c79af275a565c682ec10851ec6f63a
Geraruiz89 <guruizg@desarrollo-ultrasist.com.mx>
2023-04-23 08:16:06 -0600

    run.sh editado

--- chambea-ya-back/README.md
+++
@@ -1,3 +0,0 @@
-# desarrollo-ejercicio
-desarrollo ejercicio
-<img width="802" alt="imagen" src="https://user-images.githubusercontent.com/17259655/220720568-494fae34-b70b-46a5-a86f-147128088d71.png">
--- chambea-ya-back/run.sh
+++ chambea-ya-back/run.sh
@@ -1,6 +1,10 @@
-curl  POST 'https://access.qbits.mx/api/login' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "user":"gus@aol.com",
-    "cred":"tavo"
-}'
+docker stop segundo-examen
+docker rm segundo-examen
+
+docker pull ghcr.io/geraruiz89/chambea-ya-back
+
+docker run -d \
+--name=chambea-ya-back \
+--restart=unless-stopped \
+-p9090:8080 \
+ghcr.io/geraruiz89/chambea-ya-back
